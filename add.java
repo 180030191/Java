9948b440e88354c9f65152092074c6b190e6a13e
180030191 <79847370+180030191@users.noreply.github.com>
2021-03-01 18:51:33 +0530

    Create add.java

---
+++ add.java
@@ -0,0 +1,10 @@
+class add {
+
+   public static void main(String[] args) {
+        
+      int num1 = 51, num2 = 35, sum;
+      sum = num1 + num2;
+
+      System.out.println("Sum of these numbers: "+sum);
+   }
+}
